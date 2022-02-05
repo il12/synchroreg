4d5814a3da5e6ab880c6c5740a994938ea9b05fb
il12 <il5498@yandex.ru>
2022-02-05 17:11:09 +0300

    Fix path to db

--- services/db/getFileCompetitionById.js
+++ services/db/getFileCompetitionById.js
@@ -3,7 +3,7 @@ import { open } from 'sqlite'
 
 async function getFileCompetitionById(id){
     const db = await open({
-        filename: 'C:\\Users\\Игорь\\WebstormProjects\\synchroreg\\assets\\database.db',
+        filename: 'assets/database.db',
         driver: sqlite3.Database
     })
     const result = await db.get('SELECT file FROM competition WHERE id = ?;',
